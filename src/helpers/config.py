db37386e03a6cce56f61c70a01a85c12d7fccf73
Fedy <fedi.bn.mahfoudh@gmail.com>
2025-03-17 22:38:06 +0100

    Save local changes before merge

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,8 +1,10 @@
-from pydantic_settings import BaseSettings,SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+
     APP_NAME: str
     APP_VERSION: str
+    OPENAI_API_KEY: str
 
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
@@ -10,7 +12,6 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
-
 
 def get_settings():
     return Settings()
